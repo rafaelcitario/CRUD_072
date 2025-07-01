c22306bf777eed2c75640040806d0fa5e6004d62
Rafael Citario <contato.rafaelgomes@outlook.com>
2025-07-01 00:14:22 -0300

    chore: update seed file, add transaction, population and rollbacks. Turn safe the initial popation at database

--- mysql-init/seed.ts
+++ mysql-init/seed.ts
@@ -1,6 +1,7 @@
 import { _env } from '@src/env';
 import { generateBinaryID } from '@src/util/generate_binaryId';
-import { createConnection } from 'mysql2';
+import { createHash } from 'crypto';
+import { createConnection, QueryError } from 'mysql2/promise';
 
 ( async function () {
     const cnn = await createConnection( {
@@ -10,7 +11,60 @@ import { createConnection } from 'mysql2';
         password: _env.DB_PASSWORD,
     } );
 
-    const userId = generateBinaryID();
-    const envetId = generateBinaryID();
-} )();
 
+    const users = [
+        { email: "seed01@email.com.br", password: "user_pass_hash01" },
+        { email: "seed02@email.com.br", password: "user_pass_hash02" },
+        { email: "seed03@email.com.br", password: "user_pass_hash03" },
+        { email: "seed04@email.com.br", password: "user_pass_hash04" },
+        { email: "seed05@email.com.br", password: "user_pass_hash05" },
+    ];
+
+    try {
+        await cnn.beginTransaction();
+        for ( const user of users ) {
+            const userId = generateBinaryID();
+            const envetId = generateBinaryID();
+
+            const hash = createHash( 'sha512' );
+            const password_hash = hash.update( user.password ).digest( 'base64url' );
+
+            await cnn.execute(
+                `INSERT INTO 
+                users (id, email, password_hash, is_valid_email)
+                VALUES (?, ?, ?, ?)`,
+                [
+                    userId,
+                    user.email,
+                    password_hash,
+                    1
+                ]
+            );
+
+            await cnn.execute(
+                `INSERT INTO 
+                events (id, title, event_date, is_expired, user_id)
+                VALUES (?, ?, ?, ?, ?)`,
+                [
+                    envetId,
+                    `Evento de ${user.email}`,
+                    '2025-06-30',
+                    new Date() > new Date( '2025-06-30' ) ? 1 : 0,
+                    userId
+                ]
+            );
+        }
+
+        await cnn.commit();
+        console.log( "🌱 Seed succefuly" );
+    } catch ( err ) {
+        await cnn.rollback();
+        const typedError = err as Error;
+        throw new Error( `Database Rollback, error to populate with seed\n ${typedError.stack}` );
+    } finally {
+        await cnn.end();
+    }
+} )().catch( ( err ) => {
+    const typedError = err as Error;
+    throw new Error( `Error to populate database\n ${typedError.stack}` );
+} );
